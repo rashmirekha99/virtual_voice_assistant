df8bdf28115cbb408fa3715da2e207e03c86424a
rashmirekha99 <kothalawalarr.19@uom.lk>
2025-01-23 16:24:24 +0530

    home page

---
+++ lib/view/pages/home_page.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:voice_assistant/core/constant/image_constant.dart';
+import 'package:voice_assistant/core/constant/text_constant.dart';
+import 'package:voice_assistant/routes/route_names.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(TextConstant.appBarTitle),
+        centerTitle: true,
+      ),
+      body: Column(
+        children: [
+          Image.asset(ImageConstant.botImage),
+          ElevatedButton(
+              style: const ButtonStyle(
+                  padding: WidgetStatePropertyAll(
+                      EdgeInsets.symmetric(horizontal: 50))),
+              onPressed: () {
+                Navigator.of(context).pushNamed(RouteNames.assistantPage);
+              },
+              child: Text(
+                TextConstant.homePageButtonText,
+                style: Theme.of(context).primaryTextTheme.bodyMedium,
+              ))
+        ],
+      ),
+    );
+  }
+}
